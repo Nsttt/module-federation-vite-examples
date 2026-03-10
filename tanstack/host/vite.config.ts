6a04cccfcf24961e09289ba6fe8a32250fcfd40d
gioboa <giorgiob.boa@gmail.com>
2026-03-10 23:25:48 +0100

    feat: add TanStack example 🌴

---
+++ tanstack/host/vite.config.ts
@@ -0,0 +1,42 @@
+import { federation } from "@module-federation/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { dependencies } from "./package.json";
+
+export default defineConfig(() => ({
+  server: { fs: { allow: [".", "../shared"] } },
+  build: {
+    target: "chrome89",
+  },
+  plugins: [
+    TanStackRouterVite(),
+    federation({
+      dts: true,
+      dev: { disableDynamicRemoteTypeHints: true },
+      name: "host",
+      remotes: {
+        remote: {
+          type: "module",
+          name: "remote",
+          entry: "http://localhost:4174/remoteEntry.js",
+          entryGlobalName: "remote",
+          shareScope: "default",
+        },
+      },
+      exposes: {},
+      filename: "remoteEntry.js",
+      shared: {
+        react: {
+          requiredVersion: dependencies.react,
+          singleton: true,
+        },
+        "react-dom": {
+          requiredVersion: dependencies["react-dom"],
+          singleton: true,
+        },
+      },
+    }),
+    react(),
+  ],
+}));
---
+++ tanstack/remote/vite.config.ts
@@ -0,0 +1,55 @@
+import { federation } from "@module-federation/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { writeFileSync } from "fs";
+import { defineConfig, loadEnv } from "vite";
+import { dependencies } from "./package.json";
+
+export default defineConfig(({ mode }) => {
+  const selfEnv = loadEnv(mode, process.cwd());
+  return {
+    server: {
+      fs: {
+        allow: [".", "../shared"],
+      },
+    },
+    build: {
+      target: "chrome89",
+    },
+    plugins: [
+      TanStackRouterVite(),
+      {
+        name: "generate-environment",
+        options: function () {
+          console.info("selfEnv", selfEnv);
+          writeFileSync(
+            "./src/environment.ts",
+            `export default ${JSON.stringify(selfEnv, null, 2)};`
+          );
+        },
+      },
+      federation({
+        dts: true,
+        dev: { disableDynamicRemoteTypeHints: true },
+        filename: "remoteEntry.js",
+        name: "remote",
+        exposes: {
+          "./remote-app": "./src/App.tsx",
+        },
+        remotes: {},
+        shared: {
+          react: {
+            requiredVersion: dependencies.react,
+            singleton: true,
+          },
+          "react/": {},
+          "react-dom": {
+            requiredVersion: dependencies["react-dom"],
+            singleton: true,
+          },
+        },
+      }),
+      react(),
+    ],
+  };
+});
