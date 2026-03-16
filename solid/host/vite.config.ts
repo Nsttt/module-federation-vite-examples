da9beee56d8b9f44630926ef6f165bf1d188231a
gioboa <giorgiob.boa@gmail.com>
2026-03-16 11:29:57 +0100

    feat: add Solid example 🎉

---
+++ solid/host/vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { federation } from "@module-federation/vite";
+import { dependencies } from "./package.json";
+// import devtools from 'solid-devtools/vite';
+
+export default defineConfig({
+  plugins: [
+    federation({
+      dts: true,
+      dev: { disableDynamicRemoteTypeHints: true },
+      name: "host",
+      remotes: {
+        remote: {
+          type: "module",
+          name: "remote",
+          entry: "http://localhost:4174/remoteEntry.js",
+          entryGlobalName: "remote",
+          shareScope: "default",
+        },
+      },
+      exposes: {},
+      filename: "remoteEntry.js",
+      shared: {
+        "solid-js": {
+          requiredVersion: dependencies["solid-js"],
+          singleton: true,
+        },
+      },
+    }),
+    /*
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
+    // devtools(),
+    solidPlugin(),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: "chrome89",
+  },
+});
---
+++ solid/remote/vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { federation } from "@module-federation/vite";
+import { dependencies } from "./package.json";
+// import devtools from 'solid-devtools/vite';
+
+export default defineConfig({
+  plugins: [
+    federation({
+      dts: true,
+      dev: { disableDynamicRemoteTypeHints: true },
+      filename: "remoteEntry.js",
+      name: "remote",
+      exposes: {
+        "./remote-app": "./src/App.tsx",
+      },
+      remotes: {},
+      shared: {
+        "solid-js": {
+          requiredVersion: dependencies["solid-js"],
+          singleton: true,
+        },
+      },
+    }),
+    /*
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
+    // devtools(),
+    solidPlugin(),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: "chrome89",
+  },
+});
